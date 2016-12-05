85431c193d8de4b673523ee568156c9bbdb66dfa
Andrey <aashmele@tpchel.ru>
2016-12-05 12:17:25 +0500

    5 graphs

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,7 +14,11 @@
 
 int a=0;
 int b=0;
-
+int color1rgb[]={rand()%245+10, rand()%245+10, rand()%245+10};
+int color2rgb[]={rand()%245+10, rand()%245+10, rand()%245+10};
+int color3rgb[]={rand()%245+10, rand()%245+10, rand()%245+10};
+int color4rgb[]={rand()%245+10, rand()%245+10, rand()%245+10};
+int color5rgb[]={rand()%245+10, rand()%245+10, rand()%245+10};
 
 QVector<double> xx1;
 QVector<double> yy1;
@@ -24,13 +28,13 @@ QVector<double> yy2;
 
 QVector<double> xx3;
 QVector<double> yy3;
-/*
+
 QVector<double> xx4;
 QVector<double> yy4;
 
 QVector<double> xx5;
 QVector<double> yy5;
-
+/*
 QVector<double> xx6;
 QVector<double> yy6;
 
@@ -63,9 +67,13 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(timer3, SIGNAL(timeout()), this, SLOT(updategraph()));
 
     timer->start(1000);
-    timer2->start(40);
+    timer2->start(500);
     timer3->start(100);
 
+    ui->customPlot->xAxis->setRange(-8, 200);
+    ui->customPlot->yAxis->setRange(-5, 100);
+
+/*
     int n = 50; // number of points in graph
     double xScale = (rand()/(double)RAND_MAX + 0.5)*2;
     double yScale = (rand()/(double)RAND_MAX + 0.5)*2;
@@ -98,7 +106,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->customPlot->replot();
 
     ui->customPlot->addGraph(); // blue line
-    ui->customPlot->graph(0)->setPen(QPen(QColor(40, 110, 255)));
+    ui->customPlot->graph(0)->setPen(QPen(QColor(40, 110, 255)));*/
 }
 
 MainWindow::~MainWindow()
@@ -147,6 +155,57 @@ void MainWindow::updatepicture()
     p.end();
     ui->label->setPixmap(rotate);
     a++;*/
+    QPen graphPen;
+
+    ui->customPlot->addGraph();
+    ui->customPlot->graph()->setName("graph #1");
+//    ui->customPlot->graph()->setLineStyle((QCPGraph::LineStyle)(1));
+    ui->customPlot->graph()->setData(xx1, yy1);
+
+
+    graphPen.setColor(QColor(color1rgb[0],color1rgb[1],color1rgb[2]));
+//    graphPen.setWidthF(rand()/(double)RAND_MAX*2+1);
+    ui->customPlot->graph()->setPen(graphPen);
+
+
+    ui->customPlot->addGraph();
+//    ui->customPlot->graph()->setLineStyle((QCPGraph::LineStyle)(1));
+    ui->customPlot->graph()->setData(xx2, yy2);
+    graphPen.setColor(QColor(color2rgb[0],color2rgb[1],color2rgb[2]));
+//    graphPen.setWidthF(rand()/(double)RAND_MAX*2+1);
+    ui->customPlot->graph()->setPen(graphPen);
+
+
+
+    ui->customPlot->addGraph();
+//    ui->customPlot->graph()->setLineStyle((QCPGraph::LineStyle)(1));
+    ui->customPlot->graph()->setData(xx3, yy3);
+    graphPen.setColor(QColor(color3rgb[0],color3rgb[1],color3rgb[2]));
+//    graphPen.setWidthF(rand()/(double)RAND_MAX*2+1);
+    ui->customPlot->graph()->setPen(graphPen);
+
+
+
+    ui->customPlot->addGraph();
+//    ui->customPlot->graph()->setLineStyle((QCPGraph::LineStyle)(1));
+    ui->customPlot->graph()->setData(xx4, yy4);
+    graphPen.setColor(QColor(color4rgb[0],color4rgb[1],color4rgb[2]));
+//    graphPen.setWidthF(rand()/(double)RAND_MAX*2+1);
+    ui->customPlot->graph()->setPen(graphPen);
+
+
+
+    ui->customPlot->addGraph();
+//    ui->customPlot->graph()->setLineStyle((QCPGraph::LineStyle)(1));
+    ui->customPlot->graph()->setData(xx5, yy5);
+    graphPen.setColor(QColor(color5rgb[0],color5rgb[1],color5rgb[2]));
+//    graphPen.setWidthF(rand()/(double)RAND_MAX*2+1);
+    ui->customPlot->graph()->setPen(graphPen);
+
+
+
+
+    ui->customPlot->replot();
 }
 
 void MainWindow::updategraph()
@@ -159,40 +218,39 @@ void MainWindow::updategraph()
 
 
     xx2.append(b);
-    yy2.append(ui->dial_2->value()+qrand() % ((high + 1) - low) + low + 10);
+    yy2.append(ui->dial_3->value()+qrand() % ((high + 1) - low) + low + 10);
 
     xx3.append(b);
-    yy3.append(ui->dial_2->value()+qrand() % ((high + 1) - low) + low + 20);
+    yy3.append(ui->dial_4->value()+qrand() % ((high + 1) - low) + low + 15);
 
-    ui->customPlot->addGraph();
-    ui->customPlot->graph()->setLineStyle((QCPGraph::LineStyle)(1));
-    ui->customPlot->graph()->setData(xx1, yy1);
-    ui->customPlot->replot();
+    xx4.append(b);
+    yy4.append(ui->dial_5->value()+qrand() % ((high + 1) - low) + low + 20);
 
-    ui->customPlot->addGraph();
-    ui->customPlot->graph()->setLineStyle((QCPGraph::LineStyle)(1));
-    ui->customPlot->graph()->setData(xx2, yy2);
-    ui->customPlot->replot();
+    xx5.append(b);
+    yy5.append(ui->dial_6->value()+qrand() % ((high + 1) - low) + low + 25);
 
-
-    ui->customPlot->addGraph();
-    ui->customPlot->graph()->setLineStyle((QCPGraph::LineStyle)(1));
-    ui->customPlot->graph()->setData(xx3, yy3);
-    ui->customPlot->replot();
+//    xx6.append(b);
+//    yy6.append(ui->dial_4->value()+qrand() % ((high + 1) - low) + low + 30);
 
 
 
-    ui->customPlot->replot();
     ///////////////
     b++;
 
-
-    b++;
     if (b==200)
     {
         b=0;
         xx1.clear();
         yy1.clear();
+        xx2.clear();
+        yy2.clear();
+        xx3.clear();
+        yy3.clear();
+        xx4.clear();
+        yy4.clear();
+        xx5.clear();
+        yy5.clear();
+
         ui->customPlot->clearGraphs();
     }
 }
