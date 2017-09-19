ea289a1ec09116b8009a5795ea4ba4a9ef9a38be
Andrey <aashmele@tpchel.ru>
2017-09-19 15:07:34 +0500

    добавил 4 универсальных канала

--- stackedoptions.cpp
+++ stackedoptions.cpp
@@ -1,8 +1,20 @@
 #include "stackedoptions.h"
 #include "ui_stackedoptions.h"
 
-#define ChangeDateTimeIndex 5
 #define MainMenuIndex 0
+#define OptionsIndex 1
+#define WorkIndex 2
+#define DiagnisticIndex 3
+#define ExpertIndex 4
+#define ChangeDateTimeIndex 5
+#define ExtendedOptionsIndex 6
+#define SystemIndex 7
+#define InputsIndex 8
+#define UniversalInputsIndex 9
+#define Channel1Index 10
+#define Channel2Index 11
+#define Channel3Index 12
+#define Channel4Index 13
 
 StackedOptions::StackedOptions(QWidget *parent) :
     QDialog(parent),
@@ -11,6 +23,8 @@ StackedOptions::StackedOptions(QWidget *parent) :
 
     ui->setupUi(this);
     connect(ui->stackedWidget, SIGNAL(currentChanged(int)), this, SLOT(SetLabelIndex(int)) );
+        SetStackIndex(MainMenuIndex);
+
 }
 
 StackedOptions::~StackedOptions()
@@ -20,7 +34,7 @@ StackedOptions::~StackedOptions()
 
 void StackedOptions::on_pushButton_9_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(0);
+
 }
 
 void StackedOptions::SetLabelIndex(int newindex)
@@ -41,32 +55,32 @@ void StackedOptions::on_pushButton_10_clicked()
 
 void StackedOptions::on_pushButton_clicked()
 {
-    SetStackIndex(2);
+    SetStackIndex(WorkIndex);
 }
 
 void StackedOptions::on_pushButton_2_clicked()
 {
-    SetStackIndex(1);
+    SetStackIndex(OptionsIndex);
 }
 
 void StackedOptions::on_pushButton_12_clicked()
 {
-      SetStackIndex(MainMenuIndex);
+    SetStackIndex(MainMenuIndex);
 }
 
 void StackedOptions::on_pushButton_11_clicked()
 {
-    SetStackIndex(0);
+    SetStackIndex(MainMenuIndex);
 }
 
 void StackedOptions::on_pushButton_6_clicked()
 {
-    SetStackIndex(3);
+    SetStackIndex(DiagnisticIndex);
 }
 
 void StackedOptions::on_pushButton_7_clicked()
 {
-    SetStackIndex(4);
+    SetStackIndex(ExpertIndex);
 }
 
 void StackedOptions::on_pushButton_8_clicked()
@@ -86,5 +100,86 @@ void StackedOptions::on_ChangeDateButton_clicked()
 
 void StackedOptions::on_pushButton_15_clicked()
 {
-     SetStackIndex(MainMenuIndex);
+    SetStackIndex(MainMenuIndex);
+}
+
+void StackedOptions::on_ExtendedOptionsButton_clicked()
+{
+    SetStackIndex(ExtendedOptionsIndex);
+}
+
+void StackedOptions::on_pushButton_16_clicked()
+{
+    SetStackIndex(OptionsIndex);
+}
+
+void StackedOptions::on_ChangeDateButton_2_clicked()
+{
+    SetStackIndex(ChangeDateTimeIndex);
+}
+
+void StackedOptions::on_pushButton_3_clicked()
+{
+
+    SetStackIndex(SystemIndex);
+}
+
+void StackedOptions::on_pushButton_4_clicked()
+{
+    SetStackIndex(InputsIndex);
+}
+
+void StackedOptions::on_pushButton_20_clicked()
+{
+    SetStackIndex(ExtendedOptionsIndex);
+}
+
+void StackedOptions::on_pushButton_23_clicked()
+{
+    SetStackIndex(InputsIndex);
+}
+
+void StackedOptions::on_pushButton_21_clicked()
+{
+    SetStackIndex(Channel1Index);
+}
+
+void StackedOptions::on_pushButton_26_clicked()
+{
+    SetStackIndex(UniversalInputsIndex);
+}
+
+void StackedOptions::on_pushButton_5_clicked()
+{
+    SetStackIndex(UniversalInputsIndex);
+}
+
+void StackedOptions::on_pushButton_22_clicked()
+{
+    SetStackIndex(Channel2Index);
+}
+
+void StackedOptions::on_pushButton_24_clicked()
+{
+    SetStackIndex(Channel3Index);
+}
+
+void StackedOptions::on_pushButton_25_clicked()
+{
+    SetStackIndex(Channel4Index);
+}
+
+void StackedOptions::on_pushButton_27_clicked()
+{
+    SetStackIndex(UniversalInputsIndex);
+}
+
+void StackedOptions::on_pushButton_28_clicked()
+{
+    SetStackIndex(UniversalInputsIndex);
+}
+
+void StackedOptions::on_pushButton_29_clicked()
+{
+      SetStackIndex(UniversalInputsIndex);
 }
--- stackedoptions.h
+++ stackedoptions.h
@@ -45,6 +45,38 @@ private slots:
 
     void on_pushButton_15_clicked();
 
+    void on_ExtendedOptionsButton_clicked();
+
+    void on_pushButton_16_clicked();
+
+    void on_ChangeDateButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_20_clicked();
+
+    void on_pushButton_23_clicked();
+
+    void on_pushButton_21_clicked();
+
+    void on_pushButton_26_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_22_clicked();
+
+    void on_pushButton_24_clicked();
+
+    void on_pushButton_25_clicked();
+
+    void on_pushButton_27_clicked();
+
+    void on_pushButton_28_clicked();
+
+    void on_pushButton_29_clicked();
+
 private:
     Ui::StackedOptions *ui;
 };
